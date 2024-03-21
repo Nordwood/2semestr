134337aba1b5995d5ed5477e3f0a6023607be5a7
IzikaevADfl-31 <34055013+Nordwood@users.noreply.github.com>
2024-03-21 22:31:05 +0700

    Update legacy.cpp

--- legacy.cpp
+++ legacy.cpp
@@ -7,6 +7,8 @@ class Shape{
     public:
     double x_c, y_c;
     };
+    
+    
 class Triangle: public Shape{
     public:
     double side1len, side2len, side3len;
@@ -22,15 +24,63 @@ class Triangle: public Shape{
     
     double perimetr(){
         return side1len+side2len+side3len;
+        
     };
     
+    ~Triangle(){};
     
 };
+
+class Circle: public Shape{
+  public:  
+  double radius;
+  
+  Circle(double x_c, double y_c, double x, double y){
+      radius =  sqrt((x-x_c)*(x-x_c) + (y-y_c)*(y-y_c));
+  };
+  
+  double area(){
+      return pi*radius*radius;
+  };
+  
+  ~Circle(){};
+    
+};
+
+void test_shape(){
+Triangle tr(1, 3, 3, 6, 5,9);
+Circle cr(2,3,6,9);
+
+if (tr.perimetr() == 14.4222){
+    printf("ok\n");}
+    else{
+        printf("test failed\n");
+            }
+
+if (tr.area() == 18.3848){
+    printf("ok\n");}
+    else{
+        printf("test failed\n");
+            }
+if (cr.area() == 163.28){
+    printf("ok\n");}
+    else{
+        printf("test failed\n");
+            }
+
+
+}
+
+
+
+
 int main()
 {
-    Triangle t(2,3, 5,4, 3, 7);
-    cout<<"Triangle perimetr "<<t.perimetr();
-    cout<<"triangle area "<<t.area();
-
+    Triangle t(1,3, 3,6, 5, 9);
+    cout<<"Triangle perimetr "<<t.perimetr()<<"\n";
+    cout<<"triangle area "<<t.area()<<"\n";
+    Circle c(2,3,6,9);
+    cout<<"circle area "<<c.area()<<"\n";
+    test_shape();
     return 0;
 }
