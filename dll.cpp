2e771fcadbc1164653e7276a73358fdfd6027ee5
IzikaevADfl-31 <34055013+Nordwood@users.noreply.github.com>
2024-04-14 23:54:11 +0700

    Create dll.cpp

---
+++ dll.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+class Node {
+public:
+    int data;
+    Node* prev;
+    Node* next;
+    
+    Node(int value) : data(value), prev(nullptr), next(nullptr) {}
+};
+
+class DoublyLinkedList {
+private:
+    Node* head;
+    Node* tail;
+
+public:
+    DoublyLinkedList() : head(nullptr), tail(nullptr) {}
+
+    void insertAtEnd(int value) {
+        Node* newNode = new Node(value);
+        if (head == nullptr) {
+            head = tail = newNode;
+        } else {
+            tail->next = newNode;
+            newNode->prev = tail;
+            tail = newNode;
+        }
+    }
+
+    void display() {
+        Node* current = head;
+        while (current != nullptr) {
+            std::cout << current->data << " ";
+            current = current->next;
+        }
+        std::cout << std::endl;
+    }
+};
+
+int main() {
+    DoublyLinkedList dll;
+    
+    dll.insertAtEnd(1);
+    dll.insertAtEnd(2);
+    dll.insertAtEnd(3);
+    
+    dll.display();
+    
+    return 0;
+}
